2c067f8f46232d680ce7d31ee88ec4ccb88bcaf8
Michael Glettig <mgl@zuehlke.com>
2021-09-23 16:24:27 +0200

    - mbedtls custom configuration.
    - disable MBEDTLS_HAVE_TIME and MBEDTLS_HAVE_TIME_DATE to avoid TLS handshake error 2700 (will be fixed in later versions).
    
    # Conflicts:
    #	modules/mbedtls/configs/config-tls-generic.h
    
    # Conflicts:
    #	modules/mbedtls/configs/config-tls-generic.h

--- modules/mbedtls/configs/config-tls-generic.h
+++ modules/mbedtls/configs/config-tls-generic.h
@@ -28,9 +28,10 @@
 #define MBEDTLS_HAVE_ASM
 #endif
 
-#if defined(CONFIG_MBEDTLS_HAVE_TIME_DATE)
-#define MBEDTLS_HAVE_TIME
-#define MBEDTLS_HAVE_TIME_DATE
+/* If we build with POSIX API, automatically use time(), etc. */
+#if defined(CONFIG_POSIX_API)
+//#define MBEDTLS_HAVE_TIME
+//#define MBEDTLS_HAVE_TIME_DATE
 #endif
 
 #if defined(CONFIG_MBEDTLS_TEST)
@@ -464,6 +465,26 @@
 #define MBEDTLS_PLATFORM_ZEROIZE_ALT
 #endif
 
+
+// Custom modifications
+// for printing mbedTLS keys and certificates
+#define MBEDTLS_PEM_WRITE_C
+#define MBEDTLS_PK_WRITE_C
+#define MBEDTLS_BASE64_C
+// for key generation
+#define MBEDTLS_PK_C
+#define MBEDTLS_ECP_C
+#define MBEDTLS_ECP_DP_SECP256R1_ENABLED
+#define MBEDTLS_OID_C
+#define MBEDTLS_ASN1_WRITE_C
+
+#define MBEDTLS_ENTROPY_C
+
+// For CSR
+#define MBEDTLS_X509_CSR_WRITE_C
+#define MBEDTLS_ECDSA_C
+#define MBEDTLS_ECDH_C
+#define MBEDTLS_X509_CREATE_C
 #include "mbedtls/check_config.h"
 
 #endif /* MBEDTLS_CONFIG_H */
