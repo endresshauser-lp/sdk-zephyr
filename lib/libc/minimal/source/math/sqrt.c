754707f4299ddf3ec334f7b7a50c4c0551273805
Andrey Dodonov <Andrey.Dodonov@endress.com>
2023-04-24 16:33:29 +0200

    Use precise sqrt implementation

--- lib/libc/minimal/source/math/sqrt.c
+++ lib/libc/minimal/source/math/sqrt.c
@@ -6,18 +6,26 @@
 
 #include <math.h>
 
-double sqrt(double value)
+// borrowed from sqrtf of minimal libc, but with double instead of float, otherwise precision is low
+// current minimal libc sqrt implementation is not correct (i.e. low number of iterations)
+// https://github.com/zephyrproject-rtos/zephyr/blob/zephyr-v3.3.0/lib/libc/minimal/source/math/sqrt.c
+// https://github.com/zephyrproject-rtos/zephyr/issues/55962
+#define MINDIFF 2.25e-308
+double sqrt(double square)
 {
-	double sqrt = value / 3;
-	int i;
+	double root, last, diff;
 
-	if (value <= 0) {
+	root = square / 3.0;
+
+	if (square <= 0) {
 		return 0;
 	}
 
-	for (i = 0; i < 6; i++) {
-		sqrt = (sqrt + value / sqrt) / 2;
-	}
+	do {
+		last = root;
+		root = (root + square / root) / 2.0;
+		diff = root - last;
+	} while (diff > MINDIFF || diff < -MINDIFF);
 
-	return sqrt;
+	return root;
 }
