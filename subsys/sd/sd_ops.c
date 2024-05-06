a530c184f09dc814ad7a01acf9f5a70e3b4e3785
Cla Mattia Galliard <cla-mattia.galliard@zuehlke.com>
2024-05-06 15:42:53 +0200

    Fix an issue with concurrent access to the sd-card, by locking the card also for ioctl-calls. This fixes a bug, that would lead to crashes and erros, if two concurrent threads access the sd-card and at least one of them uses ioctl.

--- subsys/sd/sd_ops.c
+++ subsys/sd/sd_ops.c
@@ -756,6 +756,13 @@ int card_write_blocks(struct sd_card *card, const uint8_t *wbuf, uint32_t start_
 /* IO Control handler for SD MMC */
 int card_ioctl(struct sd_card *card, uint8_t cmd, void *buf)
 {
+	int ret;
+
+	ret = k_mutex_lock(&card->lock, K_MSEC(CONFIG_SD_LOCK_TIMEOUT));
+	if (ret) {
+		LOG_WRN("Could not get SD card mutex");
+		return -EBUSY;
+	}
 	switch (cmd) {
 	case DISK_IOCTL_GET_SECTOR_COUNT:
 		(*(uint32_t *)buf) = card->block_count;
@@ -769,9 +776,12 @@ int card_ioctl(struct sd_card *card, uint8_t cmd, void *buf)
 		 * Note that SD stack does not support enabling caching, so
 		 * cache flush is not required here
 		 */
-		return sdmmc_wait_ready(card);
+		ret = sdmmc_wait_ready(card);
+		break;
 	default:
-		return -ENOTSUP;
+		ret = -ENOTSUP;
+		break;
 	}
-	return 0;
+	k_mutex_unlock(&card->lock);
+	return ret;
 }
